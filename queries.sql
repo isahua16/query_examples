fa8e8b85c08ae2036bc597e695270833b4913696
isahua16 <isaelhuard@gmail.com>
2023-05-13 13:24:48 -0600

    added queries

--- queries.sql
+++ queries.sql
@@ -1,63 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.11.3-MariaDB, for osx10.16 (x86_64)
---
--- Host: localhost    Database: query_example
--- ------------------------------------------------------
--- Server version	10.11.3-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `city`
---
-
-DROP TABLE IF EXISTS `city`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `city` (
-  `population` int(11) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `city`
---
-
-LOCK TABLES `city` WRITE;
-/*!40000 ALTER TABLE `city` DISABLE KEYS */;
-INSERT INTO `city` VALUES
-(981280,'Edmonton',53.6316,-113.324),
-(90505,'Nanaimo',49.1659,-123.94),
-(2474,'Fermont',52.7833,-67.0833),
-(7740,'Iqaluit',63.7486,-68.5197),
-(273010,'Saskatoon',52.147,-106.647),
-(828854,'Mississauga',43.5953,-79.6406),
-(100000,'testing',-52.1324,98.1654);
-/*!40000 ALTER TABLE `city` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'query_example'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-05-13 13:17:23
+select name, population from city where population >= 1000000;
+select name, population from city where population <= 1000000 OR latitude > 20.0;
+select name, population from city where longitude < 20 AND latitude > 20;
+delete from city where population <= 1000;
+insert into city(name, population, longitude, latitude) values('to_delete', 1250, 45.39752, -45.97435);
+delete from city where name='to_delete';
+insert into city(name,population,longitude,latitude) values('test', 100000, 98.1654321, -52.1324342);
+update city set name='testing' where name='test';
